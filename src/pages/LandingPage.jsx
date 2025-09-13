cc7b34e78f3aa5d5491987b91b255440c4bfd17d
git practice <abhijith7133@gmail.com>
2025-09-14 01:40:26 +0530

    lazyLoaging add

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import Header from '../components/Header'
-import BackGround from '../components/BackGround'
-import Section from '../components/Section'
-import About from '../components/About'
-import Service from '../components/Service'
-import Projects from '../components/Projects'
-import WorkingProcess from '../components/WorkingProcess'
-import PricingPlan from '../components/PricingPlan'
-import SessionTwo from '../components/SessionTwo'
-import Questions from '../components/Questions'
-import Footer from '../components/Footer'
+import React, { Suspense, lazy } from 'react'
+
+// Lazy import components
+const Header = lazy(() => import('../components/Header'))
+const BackGround = lazy(() => import('../components/BackGround'))
+const Section = lazy(() => import('../components/Section'))
+const About = lazy(() => import('../components/About'))
+const Service = lazy(() => import('../components/Service'))
+const Projects = lazy(() => import('../components/Projects'))
+const WorkingProcess = lazy(() => import('../components/WorkingProcess'))
+const PricingPlan = lazy(() => import('../components/PricingPlan'))
+const SessionTwo = lazy(() => import('../components/SessionTwo'))
+const Questions = lazy(() => import('../components/Questions'))
+const Footer = lazy(() => import('../components/Footer'))
 
 const LandingPage = () => {
   return (
-    <div>
-        <Header/>
-        <BackGround/>
-        <Section/>
-        <About/>
-        <Service/>
-        <Projects/>
-        <WorkingProcess/>
-        <PricingPlan/>
-        <SessionTwo/>
-        <Questions/>
-        <Footer/>
-    </div>
+    <Suspense fallback={<div className="text-center p-10">Loading...</div>}>
+      <div>
+        <Header />
+        <BackGround />
+        <Section />
+        <About />
+        <Service />
+        <Projects />
+        <WorkingProcess />
+        <PricingPlan />
+        <SessionTwo />
+        <Questions />
+        <Footer />
+      </div>
+    </Suspense>
   )
 }
 
